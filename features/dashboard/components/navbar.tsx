763d0789c39680d0e37c193ba68dcf162298ee3a
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-12-09 10:55:37 +0530

    successfully dsa section  is added

--- features/dashboard/components/navbar.tsx
+++ features/dashboard/components/navbar.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import NavbarRoutes from './navbar-routes'
+import MobileSidebar from './mobile-sidebar'
 
 const Navbar = () => {
   return (
     <div className='p-4 border-b h-full flex  items-center bg-white dark:bg-[#191a19] shadow-sm'>
+    <MobileSidebar/>
     <NavbarRoutes/>
     </div>
   )
